a8750d0539d0fd73a58cdbd0d4e9ec86df74d95f
tyrawalker <tyra.walker@bryan.university.edu>
2020-11-19 16:58:03 -0500

    object.js

---
+++ object.js
@@ -0,0 +1,13 @@
+var shopper = {
+    FirstName: "Peter",
+    LastName: "Piper",
+    "age": 20, 
+    Address:"Nursery Rhymes",
+    diplayingInfo:function(){
+        console.log(`${shopper.FirstName} ${shopper.LastName} from the` + this.Address);
+    }
+    
+    
+},Action = {
+    Picked: ["Peck", "of", "Pickled", "Peppers"], 
+} 
