e9c07b8e41113900491311a725df39f49214a052
tyrawalker <tyra.walker@bryanuniversity.edu>
2020-12-31 13:05:13 -0500

    week5 redo

---
+++ Week_5/array_exercise/index.js
@@ -0,0 +1,112 @@
+// Template for resubmit this week. nearly caught up. 
+//section A
+//1) Given an array of numbers, return a new array that has only the numbers that are 5 or greater.
+function greaterThanFive (arr) {}
+console.log (greaterThanFive, [3, 6, 8, 2])
+
+
+// 2) Given an array of numbers, return a new array that only includes the even numbers.
+function onlyEvens (arr) {}
+console.log (onlyEvens, [3, 6, 8, 2])
+
+//Extra Credit) Make a filtered list of all the people who are old enough to see The Matrix (17+).
+function isOldEnough (arr) {}
+console.log (isOldEnough,([
+    {name: "Angelina Jolie", age: 80}, 
+    {name: "Eric Jones", age: 2}, 
+    {name: "Paris Hilton", age: 5}, 
+    {name: "Kanye West", age: 16},
+    {name: "Bob Ziroll", age: 100} 
+]) )
+
+
+//section B
+//1) Make an array of numbers that are doubles of the first array
+function doubleUp (arr) {}
+console.log (doubleUp,[2, 5, 100] )
+
+//2) Take an array of numbers and make them strings
+function returnStrings (arr) {}
+console.log (returnStrings, [2, 5, 100])
+
+//3) Capitalize each of an array of names
+function capitalizeNames (arr) {}
+console.log (capitalizeNames, ["john", "JACOB", "jinGleHeimer", "schmidt"])
+
+//Extra Credit 1) Make an array of strings of the names
+function arrayNames (arr) {}
+console.log (arrayNames, ([
+    {name: "Angelina Jolie", age: 80}, 
+    {name: "Eric Jones", age: 2}, 
+    {name: "Paris Hilton", age: 5}, 
+    {name: "Kanye West", age: 16},
+    {name: "Bob Ziroll", age: 100} 
+]) )
+
+//Extra Credit 2) Make an array of strings of the names saying whether or not they can go to The Matrix
+function canGoToTheMovie (arr) {}
+console.log (canGoToTheMovie, ([
+    {name: "Angelina Jolie", age: 80}, 
+    {name: "Eric Jones", age: 2}, 
+    {name: "Paris Hilton", age: 5}, 
+    {name: "Kanye West", age: 16},
+    {name: "Bob Ziroll", age: 100} 
+]) )
+
+//section C
+//1) Turn an array of numbers into a total of all the numbers
+function summation (arr) {}
+console.log (summation, [1, 2, 3])
+
+//2) Turn an array of numbers into a long string of all those numbers.
+function numToString (arr){}
+console.log (numToString, [1, 2, 3])
+
+// 3) Turn an array of voter objects into a count of how many people voted
+function whoVoted (arr){}
+console.log (whoVoted, ([
+    {name:"Bob", age: 30, voted: true},
+    {name:"Jake", age:32 , voted: true},
+    {name:"Kate", age:25, voted: false},
+    {name:"Sam", age:20, voted:false},
+    {name:"Phil", age: 21, voted: true},
+    {name:"Ed", age: 55, voted: true}, 
+    {name:"Tammy", age: 54, voted: true}, 
+    {name:"Marry", age:31, voted:false}, 
+    {name: "Becky", age:43, voted: false}, 
+    {name:"Joey", age:41 , voted:true},
+    {name:"Jeff", age:30 , voted: true},
+    {name:"Zack", age:19 , voted:false}, 
+]))
+
+//Extra Credit 1) Given an array of all your wishlist items, return the total cost of all items
+function totalCost (arr){}
+console.log (totalCost, ([
+    {title: "Tesla Model s" , price: 90000}, 
+    {title:"4 carat diamond ring", price:45000}, 
+    {title:"fancy hacky sack" , price: 5}, 
+    {title:"gold fidget spinner" , price:2000}, 
+    {title: "a second tesla model s", price:90000}, 
+]))
+
+//Extra Credit 2) Given an array of arrays, flatten them into a single array
+function flatten (arr) {}
+console.log (flatten, )
+
+//section D
+//1) Sort an array from smallest number to largest
+function smallToLarge(arr){}
+console.log (smallToLarge, [1, 3, 5, 2, 90, 20])
+
+//2) Sort an array from largest number to smallest
+function largeToSmall (arr){}
+console.log (largeToSmall, [1, 3, 5, 2, 90, 20])
+
+//3) Sort an array from shortest string to longest
+function shortToLong (arr) {}
+console.log (shortToLong ["dog", "wolf", "by", "family", "eaten"])
+
+//Extra Credit) Sort an array alphabetically
+function alphabetical (arr){}
+console.log (alphabetical, ["dog", "wolf", "by", "family", "eaten"])
+
--- Week_5/index.js
+++ Week_5/index.js
@@ -2,25 +2,51 @@ var fruits = ["banana", "apple", "orange", "watermelon"];
 var vegetables = ["carrot", "tomato", "pepper", "lettuce"];
 
 //template set up for next week, im nearly caught up and will use the next two weeks off to complete everything i can and resubmit all assignments not a 100%
+ function arrExercise () {
+     vegetables.pop (); 
+     //console.log (vegetables);
+     fruits.shift(); 
+     //console.log (fruits);
+      var indexOfOrange = fruits.indexOf ("orange"); 
+     // console.log (indexOfOrange);
+     fruits.push (1);
+    // console.log (fruits);
+     //console.log (vegetables.length);
+     vegetables.push (3);
+     //console.log (vegetables); 
+     var foods = fruits.concat(vegetables);
+    //console.log (foods);
+    foods.splice (4,2);
+    //console.log (foods);
+    foods.reverse ()
+    //console.log (foods);
+     var string = foods.toString ();
+     console.log (string);
 
-function allTogetherNow ()
+        
+     
+ }
 
-//1. Remove the last item from the vegetable array.
+ arrExercise();  
 
-//2. Remove the first item from the fruit array.
+//1. Remove the last item from the vegetable array.//
 
-//3. Find the index of "orange."
+//2. Remove the first item from the fruit array.//
 
-//4. Add that number to the end of the fruit array.
+//3. Find the index of "orange."//
 
-//5.Use the length property to find the length of the vegetable array.
+//4. Add that number to the end of the fruit array.//
 
-//6. Add that number to the end of the vegetable array.
+//5.Use the length property to find the length of the vegetable array.//
 
-//7. Put the two arrays together into one array. Fruit first. Call the new Array "food".
+//6. Add that number to the end of the vegetable array.//
 
-//8. Remove 2 elements from your new array starting at index 4 with one method.
+//7. Put the two arrays together into one array. Fruit first. Call the new Array "food".//
 
-//9. Reverse your array.
+//8. Remove 2 elements from your new array starting at index 4 with one method.//
+
+//9. Reverse your array.//
 
 //10. Turn the array into a string and return it.
+
+//final result will be 3, pepper, 1, watermelon, orange, apple
